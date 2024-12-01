9228eb439d06dcfd16a02df897eebea16887fb95
Kasador <huntershaw23@gmail.com>
2024-11-30 21:24:06 -0800

    main >>> update <<< changed main color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ export default {
     theme: {
       extend: {
         colors: { // color object for defining colors for Tailwind-CSS
-          light: { // light mode
             primary: '#E76768', // Soft Red
             secondary: '#FAFAD2', // Light Yellow
             accent: '#FDFED5', // Pale Cream
@@ -16,16 +15,7 @@ export default {
             surface: '#F3F4F6', // Neutral light surface
             text: '#2B2B2B', // Dark gray
           },
-          dark: { // dark mode
-            primary: '#D66364', // Deeper Red
-            secondary: '#E4A5A5', // Muted Pinkish
-            accent: '#CCCCCC', // Gray
-            background: '#1F2937', // Dark gray
-            surface: '#374151', // Neutral dark surface
-            text: '#F9FAFB', // Light gray for text
-          },
         },
       },
-    },
     plugins: [],
   };
